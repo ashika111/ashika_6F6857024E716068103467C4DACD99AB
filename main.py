fc8b06561023ac3411e18712ea92e2bbd78f0d13
ashika111 <144222503+ashika111@users.noreply.github.com>
2023-09-08 10:44:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def Hello_World():
-  print("Hello World")
-
-
-Hello_World()
+#recursive function to calculate the factorial of a given number
+def fact(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact(n-1)
+num=int(input("enter the number :")) 3
+3
+result=fact(num)
+print("the factorial of",num,"is", result)
